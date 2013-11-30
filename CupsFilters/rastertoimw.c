1bf1e04d02741ea9445b523759d1bc79f86e37c1
Daniele Cattaneo <daniele.cattaneo@alice.it>
2013-11-30 17:50:04 +0100

    Added all output resolutions to the PPD file and improved output compression.

--- CupsFilters/rastertoimw.c
+++ CupsFilters/rastertoimw.c
@@ -22,6 +22,18 @@ typedef struct {
 } job_data_t;
 
 
+int stripeIsEmpty(const uint8_t stripe[], int charWidth, int height) {
+  int i, j, k;
+  
+  k=0;
+  for (i=0; i<height; i++)
+    for (j=0; j<charWidth; j++)
+      if (stripe[k++] != 0) return 0;
+  
+  return 1;
+}
+
+
 int main(int argc, const char *argv[]) {
   ppd_file_t *ppd;
   cups_raster_t *ras;
@@ -46,11 +58,11 @@ int main(int argc, const char *argv[]) {
     ppdMarkDefaults(ppd);
     cupsMarkOptions(ppd, job.num_options, job.options);
   } else
-    fprintf(stderr, "ERROR: Unable to open PPD file: %s", strerror(errno));
+    fprintf(stderr, "ERROR: Unable to open PPD file: %s\n", strerror(errno));
   
   if (argc == 7) {
     if ((rasterfile = open(argv[6], O_RDONLY)) == -1) {
-      fprintf(stderr, "ERROR: Unable to open raster file - %s", strerror(errno));
+      fprintf(stderr, "ERROR: Unable to open raster file - %s\n", strerror(errno));
       return (1);
     }
   } else
@@ -65,9 +77,10 @@ int main(int argc, const char *argv[]) {
     uint8_t *curStripe;
     int y, row;
     int hgr;
+    int lineskip;
     
     if (pagehdr.cupsBitsPerPixel > 1) {
-      fprintf(stderr, "ERROR: Color page not supported!");
+      fprintf(stderr, "ERROR: Color page not supported!\n");
       return 1;
     }
     hgr = (pagehdr.HWResolution[1] == 144);
@@ -81,30 +94,51 @@ int main(int argc, const char *argv[]) {
     fprintf(stderr, "PAGE: %d %d\n", page, pagehdr.NumCopies);
     prnFormFeed(prn);
     row = 0;
+    lineskip = 0;
+    
     for (y=0; y<pagehdr.cupsHeight; y++) {
       cupsRasterReadPixels(ras, &curStripe[row*pagehdr.cupsBytesPerLine], pagehdr.cupsBytesPerLine);
       row++;
+      
       if (row >= 8+(8*hgr)) {
-        if (hgr)
-          hgrPrintBitmapStripeHiresH16Wk8(prn, curStripe, pagehdr.cupsBytesPerLine);
-        else {
-          prnSetLineHeight(prn, 16);
-          hgrPrintBitmapStripeH8Wk8(prn, curStripe, pagehdr.cupsBytesPerLine);
-          prnCarriageReturnLineFeed(prn);
+        if (!stripeIsEmpty(curStripe, pagehdr.cupsBytesPerLine, 8+(8*hgr))) {
+          if (lineskip > 0) {
+            prnSetLineHeight(prn, lineskip);
+            prnCarriageReturnLineFeed(prn);
+            lineskip = 0;
+          }
+          if (hgr)
+            hgrPrintBitmapStripeHiresH16Wk8(prn, curStripe, pagehdr.cupsBytesPerLine);
+          else {
+            prnSetLineHeight(prn, 16);
+            hgrPrintBitmapStripeH8Wk8(prn, curStripe, pagehdr.cupsBytesPerLine);
+            prnCarriageReturnLineFeed(prn);
+          }
+        } else {
+          lineskip += 16;
+          if (lineskip > 99) {
+            prnSetLineHeight(prn, 99);
+            prnCarriageReturnLineFeed(prn);
+            lineskip -= 99;
+          }
         }
         row = 0;
       }
+      
+      if (y * 25 % pagehdr.cupsHeight < 25)
+        fprintf(stderr, "INFO: %d%%\n", y*100/pagehdr.cupsHeight);
     }
     
     free(curStripe);
   }
   
   prnFormFeed(prn);
+  prnDealloc(prn);
   if (page == 0) {
-    fprintf(stderr, "ERROR: No pages found!");
+    fprintf(stderr, "ERROR: No pages found!\n");
     return 1;
   } else {
-    fprintf(stderr, "INFO: Ready to print.");
+    fprintf(stderr, "INFO: Ready to print.\n");
   }
   return 0;
 }
